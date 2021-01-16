f3ef8fbe272e9e37909820fe1361990d98fe7ce7
Elliott <ekrohnke@gmail.com>
2021-01-16 14:49:18 -0600

    Need to find a better guide for zillow

--- zillow.py
+++ zillow.py
@@ -9,29 +9,30 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
 #Instantiate webdriver which is in my systems PATH
-from selenium.webdriver import Edge
-driver = Edge()
+from selenium.webdriver import Chrome
+driver = Chrome()
 
-#We're going to search reddit
-driver.get("https://www.reddit.com/")
+#We're going to search zillow
+driver.get("https://www.zillow.com/")
 
-#Q is the search element for reddit.
-search = driver.find_element_by_name("q")
+#search-box-input is the search element for zillow.
+search = driver.find_element_by_class_name("Input-sc-4ry0fw-0 jbZScm react-autosuggest__input")
 
 #Input and enter scraping as a search term.
-search.send_keys("scraping")
+search.send_keys("forest lake")
 search.send_keys(Keys.RETURN)
 
 #The class name we wait for contains all the search results.
 search_results = WebDriverWait(driver, 20).until(
-EC.presence_of_element_located((By.CLASS_NAME, "rpBJOHq2PR60pnwJlUyP0"))
+EC.presence_of_element_located((By.CLASS_NAME, "list-card-info"))
 ) 
 
+#((By.CLASS_NAME, "list-card-price"))
 #Lists of posts
-posts = search_results.find_elements_by_css_selector("h3._eYtD2XCVieq6emjKBH3m")
+posts = search_results.find_elements_by_class_name("list-card-price")
 
 for post in posts:
-  header = post.find_element_by_tag_name("span")
-  print(header.text)
+  #header = post.find_element_by_tag_name("span")
+  print(post)
 
 driver.quit()
